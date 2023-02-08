47a5b2c20750a679f1f8340bb654087e8aad93c0
Mohan Dwivedi <72215741+CodeWithMohan01@users.noreply.github.com>
2023-02-08 23:53:30 +0530

    revise.java

---
+++ revise.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class revise {
+    public static void main(String[] args) {
+        // System.out.println("hello, world");
+        // String greet = "Hello";
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your name: ");
+        // String name = sc.nextLine();
+
+        // System.out.print("your age: ");
+        // int age = sc.nextInt();
+
+        // System.out.print("your Sallary: ");
+        // float sallary = sc.nextFloat();
+
+        // System.out.println(greet +" "+  name);
+        // System.out.println("age "+ age);
+        // System.out.println("Sallary "+ sallary);
+
+// *****type casting******
+
+        // int num = (int)(765.0922);
+        // float a = 99.99f;
+        // System.out.println(num);
+        // int num1 = (int)a; // (float)(num);
+        // System.out.println(num1);
+        // System.out.println(a);
+        // byte b = 40;
+        // byte c =2 ;
+        // int d = b*c;
+        // System.out.println(d);
+
+        //   *****conditional Satements*****
+        boolean check = true;
+        if(check){
+            System.out.println("hello programmer");
+        }
+        else{
+            System.out.println("");
+        }
+    }
+}
