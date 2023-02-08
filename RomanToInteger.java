997d40712f3ea237b5391efd536ca5fe6263222e
Mohan Dwivedi <72215741+CodeWithMohan01@users.noreply.github.com>
2023-02-08 23:54:15 +0530

    RomanToInteger.java

---
+++ RomanToInteger.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RomanToInteger{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        
+        int I=1, V=5, X=10, c=100, d=500, M=1000;
+        
+        String s = sc.nextLine();
+
+        for(int i=0; i<s.length(); i++)
+        {
+
+        }
+
+    }
+}
+
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
